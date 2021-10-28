2425362549d65c795c30f22307b290ea23af46c9
Meagan Gabayan <201980335@psu.palawan.edu.ph>
2021-10-28 12:08:05 +0800

    done

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/phpsandbox/web2/');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6447158');
+	define('DB_PASS', 'cwhJPsXdUp');
+	define('DB_NAME', 'sql6447158');
